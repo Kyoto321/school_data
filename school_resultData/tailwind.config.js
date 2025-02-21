d54142fe437b4882f5b596dec2aefd3c1736b89d
Ayo Bankole <bankoleayodele147@gmail.com>
2025-02-21 23:00:35 +0100

    school result data

--- school_resultData/tailwind.config.js
+++ school_resultData/tailwind.config.js
@@ -1,5 +1,7 @@
+/* eslint-env node */
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+
+export default {
   content: ["./index.html", "./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
     extend: {
